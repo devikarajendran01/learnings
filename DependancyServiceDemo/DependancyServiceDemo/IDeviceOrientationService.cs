6743cde884f31954049cc2d39a4c5b413f8157e3
Devika Rajendran <devikarajendran@hotmail.com>
2020-03-11 18:54:07 -0400

    DependancyService Demo - Xmarin added

---
+++ DependancyServiceDemo/DependancyServiceDemo/IDeviceOrientationService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Internals;
+
+namespace DependancyServiceDemo
+{
+    public interface IDeviceOrientationService
+    {
+        DeviceOrientation GetOrientation();
+    }
+}
