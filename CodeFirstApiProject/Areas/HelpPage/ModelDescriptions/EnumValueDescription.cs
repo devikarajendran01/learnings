14b0bf5ceb529767319f01b341d58b58ea75cc70
Devika Rajendran <devikarajendran@hotmail.com>
2020-03-11 18:40:57 -0400

    API with entity frsmework - code first workflow

---
+++ CodeFirstApiProject/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace CodeFirstApiProject.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
---
+++ CodeFirstApiProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CodeFirstApiProject.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
